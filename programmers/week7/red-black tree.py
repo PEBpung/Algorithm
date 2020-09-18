8a938355828968f4510b6950cdf1c3962dfdee54
kimin <mcuwwa29@gmail.com>
2020-09-18 13:38:56 +0900

    week7

---
+++ programmers/week7/red-black tree.py
@@ -0,0 +1,165 @@
+import sys
+
+class Node():
+    def __init__(self, data):
+        self.data = data  # holds the key
+        self.parent = None #pointer to the parent
+        self.left = None # pointer to left child
+        self.right = None #pointer to right child
+        self.color = 1 # 1 . Red, 0 . Black
+
+class RedBlackTree():
+    def __init__(self):
+        self.TNULL = Node(0)
+        self.TNULL.color = 0
+        self.TNULL.left = None
+        self.TNULL.right = None
+        self.root = self.TNULL
+
+    def __print_helper(self, node, indent, last):
+        # print the tree structure on the screen
+        if node != self.TNULL:
+            sys.stdout.write(indent)
+            if last:
+                sys.stdout.write("R----")
+                indent += "     "
+            else:
+                sys.stdout.write("L----")
+                indent += "|    "
+
+            s_color = "RED" if node.color == 1 else "BLACK"
+            print(str(node.data) + "(" + s_color + ")")
+            self.__print_helper(node.left, indent, False)
+            self.__print_helper(node.right, indent, True)
+
+        # fix the red-black tree
+    def  __fix_insert(self, k):
+        while k.parent.color == 1:
+            if k.parent == k.parent.parent.right:
+                u = k.parent.parent.left # uncle
+                if u.color == 1:
+                    # case 3.1
+                    u.color = 0
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.left:
+                        # case 3.2.2
+                        k = k.parent
+                        self.right_rotate(k)
+                    # case 3.2.1
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    self.left_rotate(k.parent.parent)
+            else:
+                u = k.parent.parent.right # uncle
+
+                if u.color == 1:
+                    # mirror case 3.1
+                    u.color = 0
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.right:
+                        # mirror case 3.2.2
+                        k = k.parent
+                        self.left_rotate(k)
+                    # mirror case 3.2.1
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    self.right_rotate(k.parent.parent)
+            if k == self.root:
+                break
+        self.root.color = 0
+
+    # rotate left at node x
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.TNULL:
+            y.left.parent = x
+
+        y.parent = x.parent
+        if x.parent == None:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    # rotate right at node x
+    def right_rotate(self, x):
+        y = x.left
+        x.left = y.right
+        if y.right != self.TNULL:
+            y.right.parent = x
+
+        y.parent = x.parent
+        if x.parent == None:
+            self.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    # insert the key to the tree in its appropriate position
+    # and fix the tree
+    def insert(self, key):
+        # Ordinary Binary Search Insertion
+        node = Node(key)
+        node.parent = None
+        node.data = key
+        node.left = self.TNULL
+        node.right = self.TNULL
+        node.color = 1 # new node must be red
+
+        y = None
+        x = self.root
+
+        while x != self.TNULL:
+            y = x
+            if node.data < x.data:
+                x = x.left
+            else:
+                x = x.right
+
+        # y is parent of x
+        node.parent = y
+        if y == None:
+            self.root = node
+        elif node.data < y.data:
+            y.left = node
+        else:
+            y.right = node
+
+        # if new node is a root node, simply return
+        if node.parent == None:
+            node.color = 0
+            return
+
+        # if the grandparent is None, simply return
+        if node.parent.parent == None:
+            return
+
+        # Fix the tree
+        self.__fix_insert(node)
+    # print the tree structure on the screen
+    def pretty_print(self):
+        self.__print_helper(self.root, "", True)
+
+bst = RedBlackTree()
+bst.insert(8)
+bst.insert(18)
+bst.insert(5)
+bst.insert(15)
+bst.insert(17)
+bst.insert(25)
+bst.insert(40)
+bst.insert(80)
+bst.pretty_print()
