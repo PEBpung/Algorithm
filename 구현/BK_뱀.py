49940ca2a71a90a675cebc9c0fe80e08f7a0db42
PEBpung <mcuwwa29@gmail.com>
2020-12-30 01:26:40 +0900

    [Hold]: tail 수정 필요

---
+++ 구현/BK_뱀.py
@@ -0,0 +1,130 @@
+'''
+https://www.acmicpc.net/problem/3190
+
+문제
+ 'Dummy' 라는 도스게임이 있다. 이 게임에는 뱀이 나와서 기어다니는데, 사과를 먹으면 뱀 길이가 늘어난다. 
+ 뱀이 이리저리 기어다니다가 벽 또는 자기자신의 몸과 부딪히면 게임이 끝난다.
+
+게임은 NxN 정사각 보드위에서 진행되고, 몇몇 칸에는 사과가 놓여져 있다. 보드의 상하좌우 끝에 벽이 있다. 
+게임이 시작할때 뱀은 맨위 맨좌측에 위치하고 뱀의 길이는 1 이다. 뱀은 처음에 오른쪽을 향한다.
+
+뱀은 매 초마다 이동을 하는데 다음과 같은 규칙을 따른다.
+
+먼저 뱀은 몸길이를 늘려 머리를 다음칸에 위치시킨다.
+만약 이동한 칸에 사과가 있다면, 그 칸에 있던 사과가 없어지고 꼬리는 움직이지 않는다.
+만약 이동한 칸에 사과가 없다면, 몸길이를 줄여서 꼬리가 위치한 칸을 비워준다. 즉, 몸길이는 변하지 않는다.
+사과의 위치와 뱀의 이동경로가 주어질 때 이 게임이 몇 초에 끝나는지 계산하라.
+
+입력
+첫째 줄에 보드의 크기 N이 주어진다. (2 ≤ N ≤ 100) 다음 줄에 사과의 개수 K가 주어진다. (0 ≤ K ≤ 100)
+
+다음 K개의 줄에는 사과의 위치가 주어지는데, 첫 번째 정수는 행, 두 번째 정수는 열 위치를 의미한다. 
+사과의 위치는 모두 다르며, 맨 위 맨 좌측 (1행 1열) 에는 사과가 없다.
+
+다음 줄에는 뱀의 방향 변환 횟수 L 이 주어진다. (1 ≤ L ≤ 100)
+
+다음 L개의 줄에는 뱀의 방향 변환 정보가 주어지는데,  정수 X와 문자 C로 이루어져 있으며. 
+게임 시작 시간으로부터 X초가 끝난 뒤에 왼쪽(C가 'L') 또는 오른쪽(C가 'D')로 90도 방향을 회전시킨다는 뜻이다. 
+X는 10,000 이하의 양의 정수이며, 방향 전환 정보는 X가 증가하는 순으로 주어진다.
+
+출력
+첫째 줄에 게임이 몇 초에 끝나는지 출력한다.
+
+예제 입력 1 
+6
+3
+3 4
+2 5
+5 3
+3
+3 D
+15 L
+17 D
+예제 출력 1 
+
+# tali 부분 수정 필요
+'''
+import math
+
+def R(x):
+    return [[round(math.cos(x)), -round((math.sin(x)))],[round(math.sin(x)), round(math.cos(x))]]
+
+def Rotation(order, vector):
+    result = []
+    if order == 'L': #left
+        rMatrix = R(math.radians(90))
+    elif order == 'D': #right
+        rMatrix = R(math.radians(-90))
+
+    for i in range(len(rMatrix)):
+        tmpVal = 0
+        for j in range(len(rMatrix[0])):
+            tmpVal += rMatrix[i][j] * vector[j]
+        result.append(tmpVal)
+
+    return result
+
+def implement(point, vector, order):
+    point[0] = point[0] + vector[0]
+    point[1] = point[1] + vector[1]
+    if order != 'G':
+        vector = Rotation(order, vector)
+    return point, vector
+
+n = int(input())
+kin = int(input())
+board = [[0]*n for _ in range(n)]
+
+for k in range(kin):
+    row, col = map(int, input().split())
+    board[row-1][col-1] = 1
+
+info = {}
+lin = int(input())
+for l in range(lin):
+    k, v = map(str, input().split())
+    info[int(k)] = v
+
+point = [0,0]
+vector = [0, 1] 
+tail = []
+sec = 0
+
+while(True):
+    sec += 1
+    order = 'G'
+    if sec in info.keys():
+        order = info[sec]
+    tail.insert(0, point.copy())
+
+    point, vector = implement(point, vector, order)
+
+    if (0 > point[0] or point[0]>=n) or (0 > point[1] or point[1]>=n):
+        break
+    else:
+        if board[point[0]][point[1]] == 0:
+            tail.pop(0)
+        else:
+            board[point[0]][point[1]] = 0
+    
+    if point in tail:
+        break
+
+    print(point)
+    print(tail)
+print(sec)
+
+
+'''
+6
+5 
+1 2
+1 3
+1 4
+2 4
+2 3
+3
+3 D
+4 D
+5 D
+'''
