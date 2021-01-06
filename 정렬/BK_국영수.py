30e75ed60f573d690aa23994dd6d3b6e6711a3be
PEBpung <mcuwwa29@gmail.com>
2021-01-06 23:33:53 +0900

    [Add]: 정렬 문제

--- 정렬/BK_국영수.py
+++ 정렬/BK_국영수.py
@@ -1,68 +1,21 @@
 '''
 https://www.acmicpc.net/problem/10825
 
-예제 입력 1 
-12
-Junkyu 50 60 100
-Sangkeun 80 60 50
-Sunyoung 80 70 100
-Soong 50 60 90
-Haebin 50 60 100
-Kangsoo 60 80 100
-Donghyuk 80 60 100
-Sei 70 70 70
-Wonseob 70 70 90
-Sanghyun 70 70 80
-nsj 80 80 80
-Taewhan 50 60 90
-
-예제 출력 1 
-Donghyuk
-Sangkeun
-Sunyoung
-nsj
-Wonseob
-Sanghyun
-Sei
-Kangsoo
-Haebin
-Junkyu
-Soong
-Taewhan
+input은 여러 타입으로 받을 수 있음.
+sorted 함수는 정렬 순서를 지정해줄 수 있어서 유용. 
 '''
 import sys
-
 input = sys.stdin.readline
 
 n = int(input())
-student = {}
+student = []
 
 for _ in range(n):
-    s = input().split()
-    student[s[0]] = []
-    student[s[0]].extend(list(map(int, s[1:])))
+    [name, kor, eng, math] = map(str, input().split()) 
+    student.append([name, int(kor), int(eng), int(math)])
 
-print(student.items())
-def sort_student():
-    for i, k in enumerate(student.items()):
-        if student[i][1][0] < k[1][0]:
-            student[i], k = k, student[i]
-        elif student[i][1][0] == k[1][0]:
+student = sorted(student, key=lambda x: (-x[1], x[2], -x[3], x[0]))
 
-            if student[i][1][1] > k[1][1]:
-                student[i], k = k, student[i]
-            elif student[i][1][1] == k[1][1]:
-
-                if student[i][1][2] > k[1][2]:
-                    student[i], k = k, student[i]
-                elif student[i][1][2] == k[1][2]:
-
-                    if student[i][0] > k[0]:
-                        student[i], k = k, student[i]
-
-
-sort_student()
-
-print(student)
-
+for s in student:
+    print(s[0])
 
