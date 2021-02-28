cab971640fcc4651d0686d0db8d9db8f487a862f
PEBpung <mcuwwa29@gmail.com>
2021-02-28 16:30:23 +0900

    [Add] code that solved 10991 complete

---
+++ IO/BOJ-10991.py
@@ -0,0 +1,11 @@
+'''
+https://www.acmicpc.net/problem/10991
+'''
+
+n = int(input())
+
+for i in range(1, n + 1):
+    print(' ' * (n - i), end = '')
+    for j in range(i):
+        print('*', end = ' ')
+    print()
