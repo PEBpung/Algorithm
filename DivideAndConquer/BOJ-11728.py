92ee74d336722d7cc3c7518d4879630cbc9b9ef4
PEBpung <mcuwwa29@gmail.com>
2021-03-08 12:16:09 +0900

    [Add] solved 배열 합치기

--- DivideAndConquer/BOJ-11728.py
+++ DivideAndConquer/BOJ-11728.py
@@ -2,3 +2,10 @@
 https://www.acmicpc.net/problem/11728
 '''
 
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+result = A + B
+result = sorted(result)
+for i in result:
+    print(i, end=' ')
