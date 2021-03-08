6c1be37e6784fbb03d5f2cefe2014ad7ecdea914
PEBpung <mcuwwa29@gmail.com>
2021-03-09 07:27:13 +0900

    [Add] solved 나이순정렬

---
+++ Sorting/BOJ-10814.py
@@ -0,0 +1,12 @@
+'''
+https://www.acmicpc.net/problem/10814
+'''
+
+n = int(input())
+array = []
+for _ in range(n):
+    a, b = input().split()
+    array.append((int(a), str(b)))
+array.sort(key=lambda x: x[0])
+for i in array:
+    print(i[0], i[1])
