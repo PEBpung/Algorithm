749135d05f21aee73c3c2b552e6b99d5b84b3eb3
PEBpung <mcuwwa29@gmail.com>
2021-03-09 08:26:57 +0900

    [Add] solved 문자열분석

---
+++ Strings/BOJ-10820.py
@@ -0,0 +1,18 @@
+'''
+https://www.acmicpc.net/problem/10820
+'''
+from sys import stdin
+
+for s in stdin:
+    s = s[:-1]
+    res = [0, 0, 0, 0]
+    for i in s:
+        if i.islower():
+            res[0] += 1
+        elif i.isupper():
+            res[1] += 1
+        elif i.isdigit():
+            res[2] += 1
+        else:
+            res[3] += 1
+    print(' '.join(map(str, res)))
