872c6472133a1cdeb67ca86629dea20da0800a4c
PEBpung <mcuwwa29@gmail.com>
2021-07-16 00:55:02 +0900

    [Revise] 모의고사

--- 모의고사.py
+++ 모의고사.py
@@ -1,28 +1,23 @@
-import numpy as np
-
 def solution(answers):
-    answer = []
-    supo = [[1, 2, 3, 4, 5],
-            [2, 1, 2, 3, 2, 4, 2, 5],
-            [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    pattern1 = [1, 2, 3, 4, 5] 
+    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    pred = []
-    score = []
+    score = [0, 0, 0]
     
-    for i in range(len(supo)):
-        iter = []
-        # 문제 정답의 크기와 예측 정답 크기를 맞춘다.
-        for k in range(len(answers)):
-            iter.append(supo[i][k%len(supo[i])])
-        pred.append(iter)
-        # 정답과 예측을 비교해본다.
-        result = np.array(answers) - np.array(pred[i])
-        # 비교한 값에서 맞은 개수(0)을 카운트한다. 
-        score.append(result.tolist().count(0))
+    for i, answer in enumerate(answers):
+        # 비교해서 맞은 개수를 카운트한다. 
+        if answer == pattern1[i % len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[i % len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[i % len(pattern3)]:
+            score[2] += 1
     
     # 가장 높은 점수를 받은 사람을 출력한다.
-    for idx,s  in enumerate(score):
-        if s == max(score):
-            answer.append(idx+1)
-    
+    answer = [i + 1 for i in range(3) if score[i] == max(score)]
     return answer
+
+answers = list(map(int,input().split()))
+print(answers)
+print(solution(answers))
