236516ff21160f181c49bfa948f3f07b4f4e470d
PEBpung <mcuwwa29@gmail.com>
2021-07-29 23:09:10 +0900

    [Add] Lv2. 전화번호

--- programmers/level2/전화번호 목록.py
+++ programmers/level2/전화번호 목록.py
@@ -1,18 +1,26 @@
 '''
 https://programmers.co.kr/learn/courses/30/lessons/42577
 
-119, 97674223, 1195524421	false
+119, 97674223, 331195524421	false
 '''
-from collections import Counter
-
 def solution(phone_book):
-    n = len(min(phone_book))
-    for i in range(n, 20):
-        crop = list(map(lambda x: x[:i], phone_book))
-        prefix = Counter(crop)
-        if 2 in prefix.values():
-            return True
-    return False
+    phone_book.sort() 
+    for a in range(len(phone_book)-1):
+        if phone_book[a] in phone_book[a+1] :
+            return False
+    return True
+
+# from collections import Counter
+# 119, 97674223, 1195524421
+# 119 2, 976 1,
+# def solution(phone_book):
+#     n = len(min(phone_book))
+#     for i in range(n, 10):
+#         crop = list(map(lambda x: x[:i], phone_book))
+#         prefix = Counter(crop)
+#         if 2 in prefix.values():
+#             return True
+#     return False
 
 phone_book = list(input().split(', '))
 print(solution(phone_book))
