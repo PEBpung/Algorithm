8160fc8b1b9e5e14b85a9908657bf9c18476c13c
PEBpung <mcuwwa29@gmail.com>
2021-08-09 08:53:00 +0900

    [Add] LV2. 42747

--- programmers/level2/H-index.py
+++ programmers/level2/H-index.py
@@ -1,3 +1,7 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42747
+'''
+
 def solution(citations):
     answer = 0
     # 인용 횟수를 내림차순으로 정렬한다.
@@ -5,9 +9,6 @@ def solution(citations):
     
     for i, cit in enumerate(citations):
         if cit > i:
-            answer = i+1
-            continue
-        else: break
-            
+            answer = i + 1
         
     return answer
