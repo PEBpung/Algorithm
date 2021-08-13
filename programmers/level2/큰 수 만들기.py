019464d46de0f1c2ab58271450fe1d4cb3977588
PEBpung <mcuwwa29@gmail.com>
2021-08-14 00:58:43 +0900

    [Init] LV2. 42883

---
+++ programmers/level2/큰 수 만들기.py
@@ -0,0 +1,15 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/42883
+'''
+
+def solution(number, k):
+    answer = ''
+    return answer
+
+result = solution("1924", 2)
+# "94"
+print(result)
+
+result = solution("1231234", 3)
+# "3234"
+print(result)
