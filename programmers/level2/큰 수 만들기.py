5c8faee12f06a176254ff6fdb860680ec3daf678
PEBpung <mcuwwa29@gmail.com>
2021-08-16 16:44:01 +0900

    [Add] LV2. 42883

--- programmers/level2/큰 수 만들기.py
+++ programmers/level2/큰 수 만들기.py
@@ -3,8 +3,19 @@ https://programmers.co.kr/learn/courses/30/lessons/42883
 '''
 
 def solution(number, k):
-    answer = ''
-    return answer
+    stack = []
+    for n in number:
+        while stack and n > stack[-1] and k > 0:
+            k -= 1
+            stack.pop()
+        
+        stack.append(n)
+        
+    if k != 0:
+        stack = stack[:-k]
+        
+    return ''.join(stack)
+
 
 result = solution("1924", 2)
 # "94"
@@ -13,3 +24,12 @@ print(result)
 result = solution("1231234", 3)
 # "3234"
 print(result)
+
+'''
+# 시간초과로 인해서 실패
+from itertools import combinations 
+
+def solution(number, k):
+    val = list(map(''.join, combinations(number, len(number) - k)))
+    return max(val)
+'''
