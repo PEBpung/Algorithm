3677bd4f17d7a8f6b070b3adcaa6aa1b218e9ec8
PEBpung <mcuwwa29@gmail.com>
2021-08-18 01:29:24 +0900

    [Add] LV2. 60057

--- programmers/level2/문자열 압축.py
+++ programmers/level2/문자열 압축.py
@@ -1,11 +1,36 @@
 '''
 https://programmers.co.kr/learn/courses/30/lessons/60057
 '''
+def divide(s, length):
+    result, d = '', ''
+    p = {}
+    k = 0
+    
+    for i in range(0, len(s), length):
+        div = s[i:i+length]
+        if d != div:
+            result += div
+            # 이름을 구분하기 위함
+            k += 1
+        else:
+            name = div + str(k)
+            p.setdefault(name, 1)
+            p[name] += 1
+        d = div
+    return len(result) + len(''.join(map(str, p.values())))
 
 def solution(s):
-    answer = 0
-    return answer
+    divider = []
+    if len(s) == 1:
+        return 1
+    for i in range(1, len(s)//2 + 1):
+        divider.append(divide(s, i))
+    return min(divider)
 
-result = solution("aabbaccc")
+#result = solution("aaaaaaaaaa")
+# 7
+#print(result)
+
+result = solution("bbaabaaaab")
 # 7
 print(result)
