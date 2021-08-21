b1f7d601fbb5e1e1315dd4324b4687ad39ef24b0
PEBpung <mcuwwa29@gmail.com>
2021-08-22 00:22:50 +0900

    [Init] LV2. 17677

---
+++ programmers/level2/뉴스클러스터링.py
@@ -0,0 +1,12 @@
+'''
+https://programmers.co.kr/learn/courses/30/lessons/17677
+'''
+
+def solution(str1, str2):
+    answer = 0
+    return answer
+
+# 16384
+print(solution('FRANCE', 'french'))
+# 65536
+print(solution('E=M*C^2', 'e=m*c^2'))
